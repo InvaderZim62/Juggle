c04948fe69b66c01b039f89c663229e84b6041b1
Invader Zim <pwstern@gmail.com>
2021-08-09 13:32:28 -0700

    Setup view, scene, and camera.

--- Juggle/GameViewController.swift
+++ Juggle/GameViewController.swift
@@ -14,7 +14,37 @@ import SceneKit
 
 class GameViewController: UIViewController {
 
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    var cameraNode: SCNNode!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupScene()
+        setupCamera()
+    }
+
+    // MARK: - Setup
+    
+    private func setupView() {
+        scnView = self.view as? SCNView
+        scnView.showsStatistics = true
+        scnView.allowsCameraControl = true  // use standard camera controls with swiping
+        scnView.showsStatistics = false
+        scnView.autoenablesDefaultLighting = true
+        scnView.isPlaying = true  // prevent SceneKit from entering a "paused" state, if there isn't anything to animate
+    }
+    
+    private func setupScene() {
+        scnScene = SCNScene()
+        scnScene.background.contents = "Background_Diffuse.png"
+        scnView.scene = scnScene
+    }
+    
+    private func setupCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scnScene.rootNode.addChildNode(cameraNode)
     }
 }
