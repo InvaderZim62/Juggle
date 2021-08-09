2b076d184075765c14f06beab066f0037c52bb06
Invader Zim <pwstern@gmail.com>
2021-08-09 16:48:14 -0700

    Add contact detection and toss ball back up when it contacts hand.

--- Juggle/GameViewController.swift
+++ Juggle/GameViewController.swift
@@ -14,6 +14,7 @@ import SceneKit
 
 struct Constants {
     static let ballRadius: CGFloat = 1
+    static let handInitialYPosition: Float = -6
 }
 
 class GameViewController: UIViewController {
@@ -24,6 +25,15 @@ class GameViewController: UIViewController {
     
     var ballNode: SCNNode!
     var handNode: SCNNode!
+    
+    var isBallInHand = false
+    var isBallInHandPast = false
+    var moveHandStartTime: TimeInterval = 0
+
+    struct ContactCategory {  // bit masks for contact detection
+        static let hand = 1 << 0
+        static let ball = 1 << 1
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,19 +47,36 @@ class GameViewController: UIViewController {
     private func addHandNode() {
         let hand = SCNBox(width: 1, height: 0.3, length: 1, chamferRadius: 0)
         hand.firstMaterial?.diffuse.contents = UIColor.white
-        let handNode = SCNNode(geometry: hand)
-        handNode.position = SCNVector3(x: 0, y: -6, z: 0)
+        handNode = SCNNode(geometry: hand)
+        handNode.position = SCNVector3(x: 0, y: Constants.handInitialYPosition, z: 0)
         handNode.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
+        handNode.physicsBody?.categoryBitMask = ContactCategory.hand
+        handNode.physicsBody?.contactTestBitMask = ContactCategory.ball
         scnScene.rootNode.addChildNode(handNode)
     }
 
     private func addBallNode() {
         let ball = SCNSphere(radius: Constants.ballRadius)
         ball.firstMaterial?.diffuse.contents = UIColor.red
-        let ballNode = SCNNode(geometry: ball)
+        ballNode = SCNNode(geometry: ball)
         ballNode.position = SCNVector3(x: 0, y: 10, z: 0)
         ballNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        ballNode.physicsBody?.restitution = 0  // no bounciness
+        ballNode.physicsBody?.categoryBitMask = ContactCategory.ball
+        ballNode.physicsBody?.contactTestBitMask = ContactCategory.hand
         scnScene.rootNode.addChildNode(ballNode)
+    }
+    
+    private func moveHand(deltaTime: Double) {
+        let frequency = 1 + 2 * deltaTime  // speeds up with time
+        let amplitude = 3.0
+        if deltaTime < 2 * Double.pi / frequency {  // one cycle
+            let deltaPosition = Float(amplitude * sin(-frequency * deltaTime))
+            handNode.position.y = Constants.handInitialYPosition + deltaPosition
+        } else {
+            isBallInHand = false
+            handNode.position.y = Constants.handInitialYPosition
+        }
     }
 
     // MARK: - Setup
@@ -61,11 +88,13 @@ class GameViewController: UIViewController {
         scnView.showsStatistics = false
         scnView.autoenablesDefaultLighting = true
         scnView.isPlaying = true  // prevent SceneKit from entering a "paused" state, if there isn't anything to animate
+        scnView.delegate = self  // needed for renderer, below
     }
     
     private func setupScene() {
         scnScene = SCNScene()
         scnScene.background.contents = "Background_Diffuse.png"
+        scnScene.physicsWorld.contactDelegate = self  // requires SCNPhysicsContactDelegate (extension, below)
         scnView.scene = scnScene
     }
     
@@ -74,5 +103,31 @@ class GameViewController: UIViewController {
         cameraNode.camera = SCNCamera()
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 30)
         scnScene.rootNode.addChildNode(cameraNode)
+    }
+}
+
+extension GameViewController: SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if isBallInHand {
+            if !isBallInHandPast {
+                moveHandStartTime = time  // save time on transistion to isBallInHand
+            }
+            moveHand(deltaTime: time - moveHandStartTime)
+        }
+        isBallInHandPast = isBallInHand
+    }
+}
+
+// detect contact between dominos, to play click sound (requires scnScene.physicsWorld.contactDelegate = self)
+extension GameViewController: SCNPhysicsContactDelegate {
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        let contactMask = contact.nodeA.physicsBody!.categoryBitMask | contact.nodeB.physicsBody!.categoryBitMask
+        switch contactMask {
+        case ContactCategory.hand | ContactCategory.ball:
+            print(".", terminator: "")
+            isBallInHand = true
+        default:
+            break
+        }
     }
 }
